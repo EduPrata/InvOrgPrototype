d429ff590774fea1284de953e6934e2e73b67bab
Eduardo Prata <eduardo.bjp@gmail.com>
2017-06-26 20:56:55 -0600

    Google api

--- app/index.js
+++ app/index.js
@@ -1,9 +1,17 @@
-import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
-require("uikit/dist/css/uikit.min.css")
+require('uikit/dist/js/uikit.min.js');
+Icons = require('uikit/dist/js/uikit.min.js');
+require('uikit/dist/css/uikit.min.css')
 
 // loads the Icon plugin
 UIkit.use(Icons);
 
 // components can be called from the imported UIkit reference
 UIkit.notification('Hello world.');
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
